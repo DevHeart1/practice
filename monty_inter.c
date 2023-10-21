fa624eab007a378c4ded4a5dc1fb311e62ac3b5a
DevHeart1 <obasipeace412@yahoo.com>
2023-10-21 09:50:36 -0700

    updated my interpreter

--- monty_inter.c
+++ monty_inter.c
@@ -19,6 +19,24 @@ int main(int argc, char argv)
 		exit(EXIT_FAILURE);
 	}
 
+	char buffer[128];
+	int line_num;
+
+	while(fget(buffer, sizeof(buffer), file) != NULL)
+	{
+		line_num++
+		char *opcode = strtok(buffer, " \t\n");
+		
+		if (opcode == NULL || strlen(opcode) == 0))
+		{
+			continue;
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction" opcode)
+		}
+	}
+
 	
 
 	return(o);
